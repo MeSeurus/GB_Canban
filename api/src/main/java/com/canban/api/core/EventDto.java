1a44e9ddfe6ebd3da243ce830106ecdddbf89a5d
Alex.Botya <alex_botya@bk.ru>
2022-07-18 22:35:00 +0300

    EventDto - почистить комментарии

--- api/src/main/java/com/canban/api/core/EventDto.java
+++ api/src/main/java/com/canban/api/core/EventDto.java
@@ -1,6 +1,5 @@
 package com.canban.api.core;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -24,10 +23,6 @@ public class EventDto {
     @Schema(description = "Пользователь ответственный за событие", required = true, example = "user1")
     private String username;
 
-    /**
-     * Вот тут вопрос, событие на один день или на несколько?
-     * Если на один, то смысла не имеет, можно дату создания запоминать просто
-     */
     @Schema(description = "Дата начала события", required = true)
     private LocalDateTime eventDate;
 
