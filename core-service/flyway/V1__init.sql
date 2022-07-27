502a0ab617089a76fb41e7955bd3361eb3d132a7
Alex.Botya <alex_botya@bk.ru>
2022-07-27 23:24:05 +0300

    иземенн SQL, добавлен event для user3

--- core-service/flyway/V1__init.sql
+++ core-service/flyway/V1__init.sql
@@ -22,7 +22,7 @@ insert into events (title, content, username, begin_date, end_date)
 values ( 'Create simple program', 'Hello World!', 'user1', '2022-06-24 01:00:00', '2022-06-24 03:00:00'),
        ( 'Create hard program', 'Hello World2!!', 'user2', '2022-06-24 01:00:00', '2022-06-24 07:00:00'),
        ( 'Create SUPER hard program', 'Hello World3!!', 'user3', '2022-06-24 01:00:00', '2022-06-24 03:00:00'),
-       ( 'Create SUPER hard program', 'Hello World4!!', 'user3', '2022-06-24 01:00:00', '2022-06-24 06:00:00'),
+       ( 'Create SUPER hard program', 'Hello World4!!', 'user3', '2022-06-24 04:00:00', '2022-06-24 06:00:00'),
        ( 'Create SUPER hard program', 'Hello World5!!', 'user4', '2022-06-24 01:00:00','2022-06-24 02:00:00');
 
 create table if not exists tasks
