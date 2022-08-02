367d9ddb735da042fe3572d620bc9fd64897e661
Stortor <storrtor@gmail.com>
2022-08-02 17:10:08 +0300

    фикс скл файла

--- analytics-service/flyway/V1__init.sql
+++ analytics-service/flyway/V1__init.sql
@@ -6,13 +6,9 @@ create table if not exists events_analytics (
     event_begin_date    date not null,
     event_end_date      date not null,
     added_usernames     varchar(300),
-    created_at      timestamp default current_timestamp,
-    updated_at      timestamp default current_timestamp
+    created_at          timestamp default current_timestamp,
+    updated_at          timestamp default current_timestamp
 );
-
-insert into events_analytics (event_id, event_title, event_username, event_begin_date, event_end_date, added_usernames)
-values (1, 'Create simple program', 'user3', '2022-06-24 01:00:00', '2022-06-24 03:00:00'),
-       (2, 'Create hard program', 'user3', '2022-06-24 01:00:00', '2022-06-24 07:00:00', 'user1');
 
 create table if not exists tasks
 (
@@ -29,6 +25,10 @@ create table if not exists tasks
     created_at              timestamp default current_timestamp,
     updated_at              timestamp default current_timestamp
 );
+
+insert into events_analytics (event_id, event_title, event_username, event_begin_date, event_end_date, added_usernames)
+values (1, 'Create simple program', 'user3', '2022-06-24 01:00:00', '2022-06-24 03:00:00'),
+       (2, 'Create hard program', 'user3', '2022-06-24 01:00:00', '2022-06-24 07:00:00', 'user1');
 
 insert into tasks (task_id, task_title, task_username, task_begin_date, task_end_date,task_actual_end_date, task_state, task_priority, task_kanban_name)
 values  (1, 'Create simple program', 'user3', '2022-06-24 01:00:00','2022-06-30 01:00:00', '2022-08-26 01:00:00', 'COMPLETE', 'HIGH', 'kanban1' ),
