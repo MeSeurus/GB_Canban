c03fd7bdf5337e7b3018d4210c59556ad6405dbe
novitskynick <novitskynick@gmail.com>
2022-08-10 14:11:43 +0300

    фиксы 3 часть. сваггер на ивентах artemis

--- api/src/main/java/com/canban/api/activemqevents/ActivationEvent.java
+++ api/src/main/java/com/canban/api/activemqevents/ActivationEvent.java
@@ -1,6 +1,7 @@
 package com.canban.api.activemqevents;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Событие, которое происходит в момент активации пользователя и передается брокеру сообщений " +
+        "для отправки в mail-service, чтобы сервис отправил этому пользователю ссылку на активацию аккаунта по email")
 public class ActivationEvent {
+    @Schema(description = "Никнейм пользователя")
     private String username;
+    @Schema(description = "Код для активации аккаунта")
     private String code;
+    @Schema(description = "Email пользователя")
     private String email;
 }
