d4c24026e4e1af25c1350a1466615ad01a1f4e66
novitskynick <novitskynick@gmail.com>
2022-08-11 12:03:32 +0300

    доделки 1

--- auth-service/src/main/java/com/canban/auth/validator/UserValidator.java
+++ auth-service/src/main/java/com/canban/auth/validator/UserValidator.java
@@ -18,16 +18,16 @@ public class UserValidator {
     public void validate(RegistrationUserDto registrationUserDto) {
         List<String> errors = new ArrayList<>();
         if (registrationUserDto.getUsername().isEmpty()) {
-            errors.add("Поле никнейм не должно быть пустым");
+            errors.add("Поле \"Никнейм\" не должно быть пустым");
         }
         if (registrationUserDto.getUsername().length() < 4) {
             errors.add("Никнейм должен содержать минимум 4 символа");
         }
         if (registrationUserDto.getFirstName().isEmpty()) {
-            errors.add("Поле имя не должно быть пустым");
+            errors.add("Поле \"Имя\" не должно быть пустым");
         }
         if (registrationUserDto.getLastName().isEmpty()) {
-            errors.add("Поле фамилия не должно быть пустым");
+            errors.add("Поле \"Фамилия\" не должно быть пустым");
         }
         if (!registrationUserDto.getPassword().matches(VALIDATE_PASSWORD)) {
             errors.add("Пароль должен содержать минимум 8 символов");
