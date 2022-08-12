a95bea06e8350ffbdfd905d9c6476859b8f9370e
Stortor <storrtor@gmail.com>
2022-08-12 18:59:35 +0300

    удалила лишнее

--- chat-service/src/main/java/com/canban/web/chat/models/MessageStatus.java
+++
@@ -1,8 +0,0 @@
-package com.canban.web.chat.models;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "Перечисления статусов сообщений")
-public enum MessageStatus {
-    RECEIVED, DELIVERED
-}
